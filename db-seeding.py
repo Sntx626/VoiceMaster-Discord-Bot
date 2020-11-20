52f05d35960cdeab6a904aa0c31805519ee23a4a
Leonard Langer <langer.leonard@gmail.com>
2020-11-20 16:05:09 +0100

    Added db-seeding.py

---
+++ db-seeding.py
@@ -0,0 +1,26 @@
+import psycopg2
+import json
+
+try:
+    conn = None
+    conn = psycopg2.connect(host=json.load(open("config.json"))["db-addr"], user="postgres", password=json.load(open("config.json"))["db-pass"])
+
+    c = conn.cursor()
+
+    # VoiceMaster
+    c.execute('create table voiceGuild(guildID bigint, ownerID bigint, voiceChannelID bigint, voiceCategoryID bigint)')
+    c.execute('create table voicechannel(userID bigint, voiceID bigint)')
+    c.execute('create table voiceusersettings(userID bigint, channelName text, channelLimit int)')
+    c.execute('create table voiceguildsettings(guildID bigint, ownerName text, channelLimit int, botmessagechannel bigint)')
+
+    conn.commit()
+
+    c.close()
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    try:
+        if conn is not None:
+            conn.close()
+    except:
+        pass
